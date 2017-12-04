8e44c65263dc04a7fde56824277db318bc2d47b0
cnacha <cnacha@gmail.com>
2017-12-04 09:39:46 +0700

    Fixed more issues from SIT

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.jks platforms\android\build\outputs\apk\android-release-unsigned.apk my-alias
 rm lifeguard.apk
+del lifeguard.apk
 zipalign -v 4 platforms\android\build\outputs\apk\android-release-unsigned.apk lifeguard.apk
 
